383cccab60295812a6c7cfbd2ac22107457b11bd
MCKRUZ <kruz79@gmail.com>
2020-03-24 12:39:30 -0400

    update

--- communicatorPlugin/communicatorPlugin.js
+++ communicatorPlugin/communicatorPlugin.js
@@ -1,20 +1,28 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['communicatorModularAMD'], factory(root));
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('communicatorModularAMD'));
-    } else {
-        root.communicatorPlugin = factory(root.communicatorModularAMD);
-    }
-})(typeof global !== 'undefined' ? global : this.window || this.global, function (lodash) {
-    var communicatorPlugin = {}; 
+// (function (root, factory) {
+//     if (typeof define === 'function' && define.amd) {
+//         define(['communicatorModularAMD'], factory(root));
+//     } else if (typeof exports === 'object') {
+//         module.exports = factory(require('communicatorModularAMD'));
+//     } else {
+//         root.communicatorPlugin = factory(root.communicatorModularAMD);
+//     }
+// })(typeof global !== 'undefined' ? global : this.window || this.global, function (lodash) {
+//     var communicatorPlugin = {}; 
 
-    communicatorPlugin.extendcommunicatorModularAMD = function(communicatorModularAMD) {
-        communicatorModularAMD.goodbye = function () {
+//     communicatorPlugin.extendcommunicatorModularAMD = function(communicatorModularAMD) {
+//         communicatorModularAMD.goodbye = function () {
+//             return "<h1>Goodbye!</h1>";
+//         };
+//     };
+
+//     return communicatorPlugin;
+
+// });
+
+define(['CommunicatorModularAMD'], function() {
+    return {
+        goodbye: function() {
             return "<h1>Goodbye!</h1>";
-        };
+        }
     };
-
-    return communicatorPlugin;
-
 });
